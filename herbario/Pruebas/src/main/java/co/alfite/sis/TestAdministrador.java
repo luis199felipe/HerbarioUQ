68986176708ff93f5f4afd695279779a5dbae6cf
neyderF <nfigueroas@uqvirtual.edu.co>
2019-05-06 19:16:15 -0500

    base de tests

--- herbario/Pruebas/src/main/java/co/alfite/sis/TestAdministrador.java
+++ herbario/Pruebas/src/main/java/co/alfite/sis/TestAdministrador.java
@@ -1,7 +1,10 @@
 package co.alfite.sis;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.Archive;
@@ -9,6 +12,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
+import co.alfite.sis.entidades.Empleado;
 import co.alfite.sis.entidades.Persona;
 
 public class TestAdministrador {
@@ -30,6 +34,30 @@ public class TestAdministrador {
 				.addAsResource("persistenceForTest.xml", "META-INF/persistence.xml")
 				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
 
+	}
+	
+	
+	public void crearEmpleado(Empleado e) {
+		entityManager.persist(e);
+	}
+
+	public void editarEmpleado(Empleado e) {
+		entityManager.merge(e);
+	}
+
+	public void eliminarEmpleado(Empleado e) {
+		entityManager.remove(e);
+	}
+
+	public Empleado buscarEmpleado(String idPersona) {
+		Empleado e = entityManager.find(Empleado.class, idPersona);
+		return e;
+	}
+
+	public List<Empleado> getAllEmpleados() {
+		Query q = entityManager.createNamedQuery(Empleado.EMPLEADO_GET_ALL);
+		List<Empleado> empleados = q.getResultList();
+		return empleados;
 	}
 
 }
